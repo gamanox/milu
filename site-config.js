333472b8eb246073c5b71b950a914adec33de0ef
Will Alvarez <will.alvarez@gmail.com>
2021-03-09 12:55:24 -0600

    init

---
+++ site-config.js
@@ -0,0 +1,103 @@
+module.exports = {
+  // App Info
+  appstore_link: "https://miluismi", // Enter App Store URL.
+  playstore_link: "https://miluismi", // Enter Google Play Store URL.
+  google_analytics_ID: "UA-47311644-5", // Enter Google Analytics ID or ""
+  presskit_download_link: "https://miluismi", // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file).
+  video_or_screenshot: "screenshot", // "screenshot" or "video"
+  app_url: "https://agalp.miluismi", // Domain of your website without path_prefix.
+  path_prefix: "/", // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name: "Mi Luismi",
+  app_price: "Free",
+  app_description: "Estás a la altura de ser el manager de Luismi?.",
+  app_keywords: ["automatic", "gatsbyjs", "app", "landing page"],
+
+  // Personal Info
+  your_name: "Netflix",
+  your_link: "https://miluismi",
+  your_city: "Saltillo",
+  email_address: "hello@miluismi",
+  linkedin_username: null,
+  facebook_username: null,
+  instagram_username: "luismiguellaserie",
+  twitter_username: "luismiguellaserie",
+  github_username: "luismiguellaserie",
+  youtube_username: null,
+
+  // Features List
+  features: [
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "magic",
+    },
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "mobile",
+    },
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "play-circle",
+    },
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "sync",
+    },
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "adjust",
+    },
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "star",
+    },
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "arrow-alt-circle-down",
+    },
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "link",
+    },
+    {
+      title: "Lleva a Luis Miguel a la fama",
+      description:
+        "Toma las decisiones correctas para que salga adelante la carrera de Luis Miguel... o no.",
+      fontawesome_icon_name: "info-circle",
+    },
+  ],
+  header_background: "rgba(0, 0, 0, 0.1)",
+  topbar_title_color: "#ffffff",
+  cover_overlay_color_rgba: "rgba(54, 59, 61, 0.8)",
+  device_color: "black", // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color: "ffffff",
+  primary_text_color: "#000",
+  content_width: "1170px",
+  font: `"Helvetica Neue", sans-serif`,
+  link_color: "#1d63ea",
+  app_title_color: "#ffffff",
+  app_price_color: "#ffffff",
+  app_description_color: "#ffffff",
+  feature_title_color: "#000000",
+  feature_text_color: "#666666",
+  feature_icons_foreground_color: "#1d63ea",
+  feature_icons_background_color: "#e6e6e6",
+  social_icons_foreground_color: "#666666",
+  social_icons_background_color: "#e6e6e6",
+  footer_text_color: "#666666",
+}
